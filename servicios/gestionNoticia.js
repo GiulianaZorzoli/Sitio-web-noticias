3a0e6e9c253def5c9b197974b7462d3560b13ae6
virginiaSchmied <schmiedvirginia@gmail.com>
2025-05-21 15:10:47 -0300

    Correcion coordenadas noticia mostrarDetalleNoticia()

--- servicios/gestionNoticia.js
+++ servicios/gestionNoticia.js
@@ -44,7 +44,7 @@ function mostrarDetalleNoticia(noticia) {
   `;
   card.appendChild(cuerpo);
 
-  if (noticia.x && noticia.y) {
+  if (noticia.coordenada_x && noticia.coordenada_y) {
     console.log(noticia.x)
     console.log(noticia.y)
     const mapaDiv = obtenerMapa(noticia.coordenada_x, noticia.coordenada_y);
